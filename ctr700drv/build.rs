c3950b87e5f335eccfd9d5f2488cf63f7db4cf8d
Andreas Dinter <andreas.dinter@systec-electronic.com>
2025-06-16 15:27:36 +0200

    ctr700drv: Link statically as in 1e8602af6e582fb28325eb57c7d8464a657d9978
    Signed-off-by: Andreas Dinter <andreas.dinter@systec-electronic.com>

--- ctr700drv/build.rs
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: LGPL-3.0-or-later
-//
-// (c) SYS TEC electronic AG, D-08468 Heinsdorfergrund, Am Windrad 2
-//     www.systec-electronic.com
-
-fn main() {
-    println!("cargo:rustc-link-lib=sysworxx_io");
-}
